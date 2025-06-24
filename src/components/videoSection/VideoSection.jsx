3ad669f711a65a93f33362025049c847967cfc1f
nafisurrahmanfahim <nafisurr93@gmail.com>
2025-06-24 20:33:18 +0600

    demo

--- src/components/videoSection/VideoSection.jsx
+++ src/components/videoSection/VideoSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import video from '../../assets/video.mp4'
+import video from '../../../public/video.mp4'
 const BackgroundVideoSection = () => {
   return (
     <section className="relative w-full h-screen overflow-hidden">
